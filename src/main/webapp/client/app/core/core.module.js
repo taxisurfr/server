6dfd2d76b74b2f6352c14f40c8c797fb57ecf204
peter <peter.hall@star-trac.de>
2017-09-02 17:52:29 +0200

    v3 migration

--- src/main/webapp/client/app/core/core.module.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular
-        .module('app.core', [
-            'ngAnimate', 'ngSanitize',
-            'ui.router', 'ngplus'
-        ]);
-})();
--- src/main/webapp/dashboard/app/route/route.module.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app.route', [
-        'app.core',
-        'app.widgets'
-    ]);
-})();
